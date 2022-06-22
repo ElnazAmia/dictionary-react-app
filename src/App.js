832f992b827236c158cc6163b8357358a89ab574
Elnaz <elnazamia@yahoo.com>
2022-06-22 11:25:49 +0200

    Edited styles

--- src/App.js
+++ src/App.js
@@ -7,11 +7,37 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"><img src={LogoBook} alt="logo" className="LogoBook img-fluid" />
-         </header>
-        <Dictionary defaultKeyword ="sunset"/>
+        <header className="App-header">
+          <img src={LogoBook} alt="logo" className="LogoBook img-fluid" />
+        </header>
+        <Dictionary defaultKeyword="sunset" />
         <footer className="App-footer text-center">
-          <small>Coded by Elnaz Amia</small>
+          <small>
+            This project was coded by{" "}
+            <a
+              href="https://confident-sinoussi-76f648.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Elnaz Amia 
+            </a> and is open-sourced on{" "}
+            <a
+              href="https://github.com/ElnazAmia"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://rainbow-dieffenbachia-339485.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </small>
         </footer>
       </div>
     </div>
